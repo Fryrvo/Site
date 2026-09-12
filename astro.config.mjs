ddadf901c9fdfd90cbcede091ff11d11aeda9d1e
Fryrvo <fryrvo@fryrvo.com>
2026-09-12 16:48:09 +0800

    fix cache tings

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,12 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import compress from 'astro-compress';
 import sitemap from '@astrojs/sitemap';
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://fryrvo.com', 
+  site: 'https://fryrvo.com',
+  output: 'server',
+  adapter: cloudflare(),
   integrations: [tailwind(), compress(), sitemap()],
 });
